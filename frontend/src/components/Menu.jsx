942760c350accaf1f10ebd1f215671afa13820a7
Sameer-mishra1 <77093405+Sameer-mishra1@users.noreply.github.com>
2024-03-06 22:55:16 +0530

    Removed Handles from Menu due to CSS conflict

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Handles } from "./Handles";
 
 export const Menu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,11 +12,10 @@ export const Menu = () => {
     const section = document.getElementById(sectionId);
 
     if (section) {
-      section.scrollIntoView({behavior: "smooth"});
+      section.scrollIntoView({ behavior: "smooth" });
       setIsOpen(false);
     }
-  }
-
+  };
 
   return (
     <>
@@ -64,17 +64,27 @@ export const Menu = () => {
         )}
         {/* Menu */}
         <div
-          className={`fixed inset-0 z-50 flex justify-center items-center backdrop-blur-xl ${
+          className={`fixed inset-0 z-50 flex flex-col justify-center items-center backdrop-blur-xl ${
             isOpen ? "" : "hidden"
           }`}
         >
-          <div className="rounded-lg p-8 text-white text-center w-4/5 max-w-sm">
+          <div className="rounded-lg p-8 text-white text-center w-4/5 max-w-sm flex justify-center">
             <ul className="flex flex-col items-center justify-center h-3/5 text-2xl font-['Poppins']">
-              <li className="py-5" onClick={() => scrollToSection("home")}>Home</li>
-              <li className="py-5" onClick={() => scrollToSection("aboutMe")}>About Me</li>
-              <li className="py-5" onClick={() => scrollToSection("contact")}>Contact</li>
-              <li className="py-5" onClick={() => scrollToSection("blogs")}>Blogs</li>
-              <li className="py-5" onClick={() => scrollToSection("downloads")}>Downloads</li>
+              <li className="py-5" onClick={() => scrollToSection("home")}>
+                Home
+              </li>
+              <li className="py-5" onClick={() => scrollToSection("aboutMe")}>
+                About Me
+              </li>
+              <li className="py-5" onClick={() => scrollToSection("contact")}>
+                Contact
+              </li>
+              <li className="py-5" onClick={() => scrollToSection("blogs")}>
+                Blogs
+              </li>
+              <li className="py-5" onClick={() => scrollToSection("downloads")}>
+                Downloads
+              </li>
             </ul>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -92,6 +102,7 @@ export const Menu = () => {
               ></path>
             </svg>
           </div>
+          {/* Add Handles in Menu */}
         </div>
       </div>
     </>
